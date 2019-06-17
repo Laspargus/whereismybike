a281d9ac7d4b873b7bd999c6c6cace4cfa56be4d
Laspargus <maxime.speroni@gmail.com>
2019-06-17 14:33:30 +0200

    add unique id to stations

--- app/controllers/initialization_controller.rb
+++ app/controllers/initialization_controller.rb
@@ -3,16 +3,19 @@ class InitializationController < ApplicationController
     response = HTTParty.get('http://api.citybik.es/v2/networks/velib', format: :plain)
     @list = JSON.parse response, symbolize_names: true
     @rest_stations = @list[:network][:stations]
-        @rest_stations.each do|station|
-         Station.create(
+
+    @rest_stations.each do|station|
+      Station.create(
          name: station[:name],
-         uid: station[:uid], 
+         uid: station[:id], 
          latitude: station[:latitude], 
          longitude: station[:longitude], 
          ebikes: station[:ebikes], 
          free_bikes: station[:free_bikes], 
          has_ebikes: station[:has_ebikes], 
          empty_slots: station[:empty_slots]
-         )
+      )
   end
+end
+
 end
