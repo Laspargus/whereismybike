b6e164fa8a121dad03d4bf9b5951c2b74f323200
Laspargus <maxime.speroni@gmail.com>
2019-06-17 14:08:11 +0200

    create bike in staging and prod

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,17 +6,29 @@ class HomeController < ApplicationController
     response = HTTParty.get('http://api.citybik.es/v2/networks/velib', format: :plain)
     @list = JSON.parse response, symbolize_names: true
     @rest_stations = @list[:network][:stations]
+        @rest_stations.each do|station|
+         Station.create(
+         name: station[:name],
+         uid: station[:uid], 
+         latitude: station[:latitude], 
+         longitude: station[:longitude], 
+         ebikes: station[:ebikes], 
+         free_bikes: station[:free_bikes], 
+         has_ebikes: station[:has_ebikes], 
+         empty_slots: station[:empty_slots]
+         )
+   end
     
-    @rest_stations.each do|station|
-      @station = Station.find_by(uid: station[:uid])
+    # @rest_stations.each do|station|
+    #   @station = Station.find_by(uid: station[:uid])
 
-      if @station.free_bikes != station[:free_bikes]
-        @station.update(
-          free_bikes: station[:free_bikes], 
-          empty_slots: station[:empty_slots]
-        )
-      end
-    end
+    #   if @station.free_bikes != station[:free_bikes]
+    #     @station.update(
+    #       free_bikes: station[:free_bikes], 
+    #       empty_slots: station[:empty_slots]
+    #     )
+    #   end
+    # end
 
     @stations = Station.where("free_bikes > 0")
     @stations = Station.near("9 rue Bergère, Paris 75002, France").take(5)
