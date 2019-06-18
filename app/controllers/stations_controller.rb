3dda9a1f7ba9681b9f44acae5633c82bbcd604fd
Laspargus <maxime.speroni@gmail.com>
2019-06-18 16:48:03 +0200

    refacto controller

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,26 +2,25 @@
 
 class StationsController < ApplicationController
   def index
-    @stations = Station.where("free_bikes > 0")
+ 
     if params[:latitude].present?
       @latitude = params[:latitude]
       @longitude = params[:longitude]
-      @stations = Station.near([@latitude, @longitude]).take(5)
     elsif params[:location].present?
       adress = Geocoder.search(params[:location])
       @latitude = adress.first.coordinates[0]
       @longitude = adress.first.coordinates[1]
-      @stations =  Station.near([@latitude, @longitude]).take(5)
     else
       @latitude = '48.872131'
       @longitude = '2.346660'
-      @stations = Station.near("9 rue Bergère, Paris 75002, France").take(5)
     end
+
+    @stations = Station.where("free_bikes > 0").near([@latitude, @longitude]).take(5)
 
     respond_to do |format|
       format.html
       format.js
     end
-    
+
   end
 end
